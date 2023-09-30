78731e3a490571e089ecec5bdb86d30fd01b11b6
fsouzadi1995 <fsouzadi1995@gmail.com>
2023-09-30 18:45:06 -0300

    chore: initial commit

---
+++ test/server.spec.js
@@ -0,0 +1,30 @@
+//Require the dev-dependencies
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../server');
+let should = require('chai').should();
+var expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('/', () => {
+  it('it should return a 200 OK response', (done) => {
+    chai
+      .request(server)
+      .get('/')
+      .end((_, res) => {
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+
+  it('it should return a hello world body response', (done) => {
+    chai
+      .request(server)
+      .get('/')
+      .end((_, res) => {
+        expect(res.text).to.eql('Hello world');
+        done();
+      });
+  });
+});
